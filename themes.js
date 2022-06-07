7dada429b9d2b93da436fc87616c119d6146d91d
Christopher <105583070+shadesofchristopher@users.noreply.github.com>
2022-06-07 17:53:58 -0400

    Add files via upload

---
+++ themes.js
@@ -0,0 +1,54 @@
+exports.COLOR_THEMES = {
+    default: {
+      colors: {
+        primary: '#010020',
+        'gradient-1': '#de7aff',
+        'gradient-2': '#1111d1',
+        'gradient-3': '#9D00FF',
+        'gradient-4': '#dc5ff5',
+      },
+    },
+    bejamas: {
+      colors: {
+        primary: '#FF8585',
+        'gradient-1': '#7d7aff',
+        'gradient-2': '#2121E2',
+        'gradient-3': '#FF76B8',
+        'gradient-4': '#001AFF',
+      },
+    },
+    netlify: {
+      colors: {
+        primary: '#00A354',
+        'gradient-1': '#00F0FF',
+        'gradient-2': '#00F0FF',
+        'gradient-3': '#FAFF00',
+        'gradient-4': '#00F0FF',
+      },
+    },
+    reddie: {
+      colors: {
+        primary: '#FF4D4D',
+        'gradient-1': '#FFC700',
+        'gradient-2': '#FF85DD',
+        'gradient-3': '#FF85DD',
+        'gradient-4': '#FF8585',
+      },
+    },
+    greenie: {
+      colors: {
+        primary: '#C78500',
+        'gradient-1': '#FFCC81',
+        'gradient-2': '#00F37F',
+        'gradient-3': '#00F37F',
+        'gradient-4': '#FFCC81',
+      },
+    },
+  };
+  
+  exports.FONT_THEMES = {
+    'sans-serif': `ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`,
+    serif: `ui-serif, Georgia, Cambria, "Times New Roman", Times, serif`,
+    monospace: `ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace`,
+  };
+  
