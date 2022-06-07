fc8b4f71c30ef1654992a81c3c56cdd251e3194b
Christopher <105583070+shadesofchristopher@users.noreply.github.com>
2022-06-07 18:02:09 -0400

    Add files via upload

---
+++ components/SEO.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import React from 'react';
+
+export default function SEO({ title, description }) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+    </Head>
+  );
+}
